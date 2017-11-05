e456d690db011ca863b6f8306bc46b4d6ec677cf
Paul Xue <xuepaul.xp@gmail.com>
2017-11-05 12:10:32 -0500

    set up feed and post models

--- src/models/feed.js
+++ src/models/feed.js
@@ -1,4 +1,39 @@
 // feed model object represents a rss feed
+import { action, observable, extendObservable } from "mobx"
 
-class Feed {}
+class Feed {
+  // some internal id field
+  @observable id
+  // name of the feed
+  @observable name
+  // description of the feed
+  @observable description
+  // the feed url
+  @observable webUrl
+
+  // timestamp of the last time
+  // this feed was viewed
+  @observable lastViewedAt
+  // timestamp of the last time
+  // this feed as updated
+  @observable lastUpdatedAt
+
+  // count of number of items
+  // unread in this feed
+  @observable unreadCount
+
+  // parsed list of posts
+  @observable posts = []
+
+  constructor(props) {
+    extendObservable(this, props)
+  }
+
+  // parseFeedUrl takes a given feed url and
+  // returns related metadata
+  //
+  // use some external lib?
+  // https://github.com/danmactough/node-feedparser
+  @action parseFeedUrl(url = "") {}
+}
 export default Feed
--- src/models/post.js
+++ src/models/post.js
@@ -1,4 +1,26 @@
 // post model object represents a single post in a feed
+import { observable, extendObservable } from "mobx"
 
-class Post {}
+class Post {
+  // some internal id field
+  @observable id
+  // title of the post
+  @observable title
+  // author name
+  @observable author
+  // short description/content if available
+  @observable blurb
+  // url link if available
+  @observable webUrl
+
+  // time posted
+  @observable postedAt
+
+  // if the post is read or not
+  @observable isRead = false
+
+  constructor(props) {
+    extendObservable(this, props)
+  }
+}
 export default Post
